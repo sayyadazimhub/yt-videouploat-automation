03b9703bd6e22edbe18f60f4377168215465ecd4
Sayyad Azim <azimsayyad90@gmail.com>
2026-08-22 14:39:17 +0530

    cors added and ffmpeg linuq vs also

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ app.use(urlencoded({ extended: true }));
 
 app.use(
     cors({
-        origin: ["http://localhost:3000", "http://localhost:3001"],
+        origin: [process.env.FRONTEND_URL, "http://localhost:3000", "http://localhost:3001"],
         credentials: true,
     })
 );
